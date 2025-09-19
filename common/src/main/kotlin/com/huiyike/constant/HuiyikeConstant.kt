5492a6542e8c7e98c773bc3df7de9c862cd6caee
zhianABC <191036020@qq.com>
2025-09-19 10:48:50 +0800

    Update API keys and secrets in HuiyikeConstant

--- common/src/main/kotlin/com/huiyike/constant/HuiyikeConstant.kt
+++ common/src/main/kotlin/com/huiyike/constant/HuiyikeConstant.kt
@@ -13,7 +13,7 @@ object HuiyikeConstant {
 
     val YUNPIAN_URL = "https://sms.yunpian.com/v2/sms/single_send.json"
 
-    val YUNPIAN_API_KEY = "3afe281cfade489500ccbc9b811914fb"
+    val YUNPIAN_API_KEY = "asdfbzxcvzxcaf"
 
     val YUNPIAN_TEMPLATE = "【会亿客】%s（验证码，十分钟内有效，若非本人操作请忽略）"
 
@@ -21,9 +21,9 @@ object HuiyikeConstant {
 
     val OSS_BUCKET_NAME = "files-jinghuiyi-cn"
 
-    val OSS_ACCESS_KEY_ID = "LTAI5tH2qdZG4d8Z2v5XatwC"
+    val OSS_ACCESS_KEY_ID = "adfasdfas"
 
-    val OSS_ACCESS_KEY_SECRET = "ZWHrzu1LsGQ0zUyzI7R1mdUs6sZihU"
+    val OSS_ACCESS_KEY_SECRET = "gdsfgsdfgqrtqerqqqq"
 
     val MDC_APP_KEY = "MDC_APP_KEY"
 }
